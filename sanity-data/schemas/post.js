4ff2a381a5035a12252a6a6663dede6f19a0f4be
xcvrys <xcvrys@gmail.com>
2022-10-12 19:38:39 +0200

    add sanity base

---
+++ sanity-data/schemas/post.js
@@ -0,0 +1,81 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: { type: 'author' },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'categories',
+      title: 'Categories(max 2 elements)',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'category' } }],
+      validation: Rule => Rule.max(2)
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'likes',
+      title: 'Likes',
+      type: 'number',
+    },
+  ],
+
+  initialValue: {
+    likes: 0
+  },
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const { author } = selection
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`,
+      })
+    },
+  },
+}
