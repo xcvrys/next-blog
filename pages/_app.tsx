7c292e1cdd7549d312ce77a220617b5ee5c082c5
xcvrys <xcvrys@gmail.com>
2022-10-15 00:58:05 +0200

    ♿

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import '../styles/globals.css'
 import { Footer } from '../components/molecules/footer';
 import { Header } from '../components/molecules/header';
 import { useRouter } from 'next/router';
-
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return ( //if path != /404, render header and footer
     <>
+      <NextNProgress color="#266150" />
       {router.pathname !== "/404" && <Header />}
       <Component {...pageProps} />
       {router.pathname !== "/404" && <Footer />}
